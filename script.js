f7524b44a0696771cd3c2759c26a9c151f5664d7
Éloi Draï <eloidrai@gmail.com>
2019-11-30 22:53:40 +0100

    Amélioration des constructeurs

--- script.js
+++ script.js
@@ -2,26 +2,25 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext('2d');
 
 class Balle {
-    constructor(){
-        this.vX = 1;
-        this.vY = 1;
-        this.X = Math.floor(Math.random()*400+50);
-        this.Y = 1;
+    constructor(canvas, rayon){
+        this.canvas = canvas, this.ctx = canvas.getContext('2d');
+        this.vX = 1, this.vY = 1;                                   // Vecteur directeur (dans le repère du canvas)
+        this.X = Math.floor(Math.random()*canvas.width), this.Y = 1;      // Coordonées de départ
+        this.rayon = rayon;
     }
     
-    dessiner(ctx){
-        ctx.fillStyle = "red";
-        ctx.beginPath();
-        ctx.arc(this.X, this.Y, 6, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fill();
+    dessiner(){
+        this.ctx.fillStyle = "red";
+        this.ctx.beginPath();
+        this.ctx.arc(this.X, this.Y, this.rayon, 0, 2 * Math.PI);
+        this.ctx.fill();
     }
     
-    avancer(planche){
-        if (this.X<0 || this.X>500){
+    avancer(planche){                                   // On met la planche en argument afin de calculer un éventuel rebond
+        if (this.X<0 || this.X>canvas.width){
             this.vX *= -1;
         }
-        if (this.Y<0 || (this.Y+6==400-30 && planche.x-50<this.X && this.X<planche.x+50)){
+        if (this.Y<0 || (this.Y+this.rayon===this.canvas.height-planche.alti && planche.X-(planche.l/2)<this.X && this.X<planche.X+planche.l/2)){
             this.vY *= -1;
         }
         this.X += this.vX;
@@ -31,34 +30,32 @@ class Balle {
 }
 
 class Planche {
-    constructor(){
-        this.x = 500/2;
+    constructor(canvas, altitude, longueur, epaisseur){
+        this.canvas = canvas, this.ctx = canvas.getContext('2d');
+        this.alti = altitude, this.l = longueur, this.ep = epaisseur;
+        this.X = canvas.width/2;
     }
     
-    dessiner(ctx){
-        ctx.fillStyle = "green";
-        ctx.beginPath();
-        ctx.rect(this.x-50, 400-30, 100, 10);
-        ctx.closePath();
-        ctx.fill();
+    dessiner(){
+        this.ctx.fillStyle = "navy";
+        this.ctx.beginPath();
+        this.ctx.rect(this.X-(this.l/2), this.canvas.height-this.alti, this.l, this.ep);
+        this.ctx.closePath();
+        this.ctx.fill();
     }
 }
 
-const b = new Balle();
-const p = new Planche();
+const b = new Balle(canvas, 6);
+const p = new Planche(canvas, 30, 100, 10);
 
 window.setInterval((t)=>{
     ctx.clearRect(0,0,canvas.width, canvas.height)
     b.avancer(p);
-    b.dessiner(ctx);
-    p.dessiner(ctx);
-},6
+    b.dessiner();
+    p.dessiner();
+},5
 );
 
 canvas.onmousemove = (e)=>{
-    p.x = e.offsetX;
+    p.X = e.offsetX;
 }
-
-/*Beurk*/
-canvas.style.border = "black 2px dashed";
-canvas.style.backgroundColor = "lightgrey";
